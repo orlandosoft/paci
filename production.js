746bce08460007950b97c40c272eeaf4b8ae7fdf
Ramon Diaz <jdiaz4@ucol.mx>
2017-11-06 15:37:10 -0600

    delete json files

--- production.js
+++
@@ -1,9 +0,0 @@
-concat: {   
-    dist: {
-        src: [
-            'js/libs/*.js', // All JS in the libs folder
-            'js/global.js'  // This specific file
-        ],
-        dest: 'js/build/production.js',
-    }
-}
